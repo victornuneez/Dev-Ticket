6b14692b2642d7a09bd70c41b51e3e92db0139a0
Victor Nuñez <victornuneezdfgh@gmail.com>
2026-07-09 10:32:34 -0300

    feat: crea el archivo constants donde centraliza el valor de los estados de la app

---
+++ src/constanst.js
@@ -0,0 +1,4 @@
+// Valores de los estados centralizados en un archivo para evitar errores y facil actualizacion.
+export const NEW = 'NEW';
+export const IN_PROGRESS = 'IN_PROGRESS';
+export const DONE = 'DONE';
