b791b3b778a898f2379dca789666d077272bf539
Victor Nuñez <victornuneezdfgh@gmail.com>
2026-07-09 15:35:23 -0300

    feat: crea el componente del formulario para crear tareas

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,24 @@
+import { createElement } from "../../framework.js";
+
+// Componente del formulario para crear tareas.
+export const TaskForm = ({ developers, onCreateTask }) => {
+    return (
+        <div>
+            <input id="title" type="text" placeholder="Titulo de la tarea" />
+            
+            <select id="developersSelect">
+                {developers.map(dev => {
+                    <option value={dev}>{dev}</option>
+                })}
+            </select>
+            
+            <button onClick={() => {
+                const title = document.getElementById('title').value;
+                const dev = document.getElementById('developersSelect').value;
+                onCreateTask(title, dev)
+            }}>
+                Crear Tarea
+            </button>
+        </div>
+    );
+};
