f007eaee71ca14bf5f596247a4d21855ed5cd3f5
Victor Nuñez <victornuneezdfgh@gmail.com>
2026-07-10 20:17:30 -0300

    fix: arregla error de sintaxis en la accion de delete_task

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,8 @@
 // Creamos y centralizamos los objetos que describen las acciones que se puede realizar en la app.
 
 import { createAction } from "../framework.js";
-import { NEW, IN_PROGRESS, DONE } from "./constanst.js";
+
 
 export const CREATE_TASK = createAction('CREATE_TASK');
 export const MOVE_TASK = createAction('MOVE_TASK');
-export const DELETE_TASK = createAction('DELETE _TASK');
+export const DELETE_TASK = createAction('DELETE_TASK');
