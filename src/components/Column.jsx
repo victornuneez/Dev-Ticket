60fbf4008aaf62319c378ace4fed31e053cd0e4b
Victor Nuñez <victornuneezdfgh@gmail.com>
2026-07-11 17:12:21 -0300

    feat: recibe y pasa como props los handlers de mover y eliminar tareas al componente TaskCard

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,13 +1,18 @@
 import { createElement } from "../../framework.js";
 import { TaskCard } from "./TaskCard.jsx";
 
-export const Column = ({ title, tasks }) => {
+export const Column = ({ title, tasks, onMoveTask, onDeleteTask }) => {
     return (
         <div className="column">
             <h2>{title}</h2>
             <div className="task-list">
                 { tasks.map(task => (
-                    <TaskCard key={task.id} task={task} />
+                    <TaskCard 
+                    key={task.id} 
+                    task={task} 
+                    onMoveTask = {onMoveTask}
+                    onDeleteTask = {onDeleteTask}
+                    />
                 ))}
             </div>
         </div>
