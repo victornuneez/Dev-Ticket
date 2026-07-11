ac8afa7071df2ae19294575823ae4bb8ad995228
Victor Nuñez <victornuneezdfgh@gmail.com>
2026-07-11 17:11:11 -0300

    feat: destrutura y pasa como props los handlers de mover y eliminar tareas

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -2,12 +2,12 @@ import { createElement } from "../../framework.js";
 import { NEW, IN_PROGRESS, DONE } from "../constanst.js";
 import { Column } from './Column.jsx'
 
-export const Board = ({ tasks }) => {
+export const Board = ({ tasks, onMoveTask, onDeleteTask }) => {
     return (
         <div className="board">
-            <Column title="Nuevos" tasks={tasks.filter(t => t.status === NEW)} />
-            <Column title="En Desarrollo" tasks={tasks.filter(t => t.status === IN_PROGRESS)} />
-            <Column title="Realizados" tasks={tasks.filter(t => t.status === DONE)} />
+            <Column title="Nuevos" tasks={tasks.filter(t => t.status === NEW)} onMoveTask = {onMoveTask} onDeleteTask = {onDeleteTask} />
+            <Column title="En Desarrollo" tasks={tasks.filter(t => t.status === IN_PROGRESS)} onMoveTask = {onMoveTask} onDeleteTask = {onDeleteTask} />
+            <Column title="Realizados" tasks={tasks.filter(t => t.status === DONE)}  onMoveTask = {onMoveTask} onDeleteTask = {onDeleteTask} />
         </div>
     );
 };
